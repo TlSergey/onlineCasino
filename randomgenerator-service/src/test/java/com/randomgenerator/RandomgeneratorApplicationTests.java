206988fd64d92228669d44554f0429cfe24900f7
TISergey <sergtel65@gmail.com>
2019-10-24 15:31:48 +0400

    initial commit

---
+++ randomgenerator-service/src/test/java/com/randomgenerator/RandomgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomgeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
